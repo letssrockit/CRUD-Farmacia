7fae3a8f6358a51f63d9340acbc968995046cc93
letssrockit <leticiarocha_22@outlook.com.br>
2024-01-31 10:15:04 -0300

    criação do CRUD do recurso Categoria

---
+++ src/main/java/com/generation/farmaciaonline/model/Categoria.java
@@ -0,0 +1,39 @@
+package com.generation.farmaciaonline.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "A categoria é obrigatória")
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
