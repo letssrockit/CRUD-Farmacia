8a1bb6110f0fde714352eeea43c7963fc2e1f482
letssrockit <leticiarocha_22@outlook.com.br>
2024-01-31 09:25:10 -0300

    Primeiro commit

---
+++ src/main/java/com/generation/farmaciaonline/FarmaciaonlineApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmaciaonline/FarmaciaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
